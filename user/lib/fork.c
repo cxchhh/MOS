0db75a979a4cfd9280239ce31f512b95d1f1540f
陈叙传 <21374067@buaa.edu.cn>
2024-06-16 17:26:21 +0800

    abl: w/o change for sig_entry

--- user/lib/fork.c
+++ user/lib/fork.c
@@ -96,14 +96,14 @@ static void duppage(u_int envid, u_int vpn) {
 static void __attribute__((noreturn)) sig_entry(struct Trapframe *tf, u_int signum, void (*handler)(int)) {
 	//debugf("%x %d in sig_entry! %x\n", env->env_id, signum,debugf);
 	int r;
-	if(handler != NULL && signum != SIGKILL){
+	if(handler != NULL){
 		handler(signum);
 	}
 	else if(signum == SIGKILL || signum == SIGINT || signum == SIGILL || signum == SIGSEGV){
         exit();
 		return;
     }
-	else{
+	else if(signum == SIGSYS){
 		tf->cp0_epc += 4;
 	}
 	r = syscall_finish_sig(0, tf);
